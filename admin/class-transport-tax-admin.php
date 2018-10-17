432155441d6a08dd36acf4c11914f0c03de6e76f
Ingniq <siqniq@bk.ru>
2018-10-17 18:01:24 +0700

    shortcode template - add fields

--- admin/class-transport-tax-admin.php
+++ admin/class-transport-tax-admin.php
@@ -118,142 +118,65 @@ class Transport_Tax_Admin {
           </div>
           <div class="input-group">
             <span class="input-group-addon">Регион</span>
-            <select id="reg" class="form-control">
-              <option value="21">Алтайский край</option>
-              <option value="27">Амурская область</option>
-              <option value="28">Архангельская область</option>
-              <option value="29">Астраханская область</option>
-              <option value="30">Белгородская область</option>
-              <option value="31">Брянская область</option>
-              <option value="32">Владимирская область</option>
-              <option value="33">Волгоградская область</option>
-              <option value="34">Вологодская область</option>
-              <option value="35">Воронежская область</option>
-              <option value="78">Еврейская автономная область</option>
-              <option value="74">Забайкальский край</option>
-              <option value="36">Ивановская область</option>
-              <option value="37">Иркутская область</option>
-              <option value="7">Кабардино-Балкарская Республика</option>
-              <option value="38">Калининградская область</option>
-              <option value="39">Калужская область</option>
-              <option value="40">Камчатский край</option>
-              <option value="9">Карачаево-Черкесская Республика</option>
-              <option value="41">Кемеровская область</option>
-              <option value="42">Кировская область</option>
-              <option value="43">Костромская область</option>
-              <option value="22">Краснодарский край</option>
-              <option value="23">Красноярский край</option>
-              <option value="44">Курганская область</option>
-              <option value="45">Курская область</option>
-              <option value="46">Ленинградская область</option>
-              <option value="47">Липецкая область</option>
-              <option value="48">Магаданская область</option>
-              <option value="76" selected>Москва</option>
-              <option value="49">Московская область</option>
-              <option value="50">Мурманская область</option>
-              <option value="80">Ненецкий автономный округ</option>
-              <option value="51">Нижегородская область</option>
-              <option value="52">Новгородская область</option>
-              <option value="53">Новосибирская область</option>
-              <option value="54">Омская область</option>
-              <option value="55">Оренбургская область</option>
-              <option value="56">Орловская область</option>
-              <option value="57">Пензенская область</option>
-              <option value="58">Пермский край</option>
-              <option value="24">Приморский край</option>
-              <option value="59">Псковская область</option>
-              <option value="1">Республика Адыгея</option>
-              <option value="4">Республика Алтай</option>
-              <option value="2">Республика Башкортостан</option>
-              <option value="3">Республика Бурятия</option>
-              <option value="5">Республика Дагестан</option>
-              <option value="6">Республика Ингушетия</option>
-              <option value="8">Республика Калмыкия</option>
-              <option value="10">Республика Карелия</option>
-              <option value="11">Республика Коми</option>
-              <option value="79">Республика Крым</option>
-              <option value="12">Республика Марий Эл</option>
-              <option value="13">Республика Мордовия</option>
-              <option value="14">Республика Саха (Якутия)</option>
-              <option value="15">Республика Северная Осетия - Алания</option>
-              <option value="16">Республика Татарстан</option>
-              <option value="17">Республика Тыва</option>
-              <option value="19">Республика Хакасия</option>
-              <option value="60">Ростовская область</option>
-              <option value="61">Рязанская область</option>
-              <option value="62">Самарская область</option>
-              <option value="77">Санкт-Петербург</option>
-              <option value="63">Саратовская область</option>
-              <option value="64">Сахалинская область</option>
-              <option value="65">Свердловская область</option>
-              <option value="84">Севастополь</option>
-              <option value="66">Смоленская область</option>
-              <option value="25">Ставропольский край</option>
-              <option value="67">Тамбовская область</option>
-              <option value="68">Тверская область</option>
-              <option value="69">Томская область</option>
-              <option value="70">Тульская область</option>
-              <option value="71">Тюменская область</option>
-              <option value="18">Удмуртская Республика</option>
-              <option value="72">Ульяновская область</option>
-              <option value="26">Хабаровский край</option>
-              <option value="81">Ханты-Мансийский автономный округ - Югра</option>
-              <option value="73">Челябинская область</option>
-              <option value="85">Чеченская Республика</option>
-              <option value="20">Чувашская Республика</option>
-              <option value="82">Чукотский автономный округ</option>
-              <option value="83">Ямало-Ненецкий автономный округ</option>
-              <option value="75">Ярославская область</option>
-            </select>
-          </div><!-- /input-group -->
-          <div class="input-group">
-            <span id="transport_sr" class="input-group-addon">Транспортное средство</span>
-            <select id="autoType" class="form-control">
-              <option value="1">Легковой автомобиль</option>
-              <option value="2">Мотоцикл, мотороллер</option>
-              <option value="3">Автобус</option>
-              <option value="4">Грузовой автомобиль</option>
-              <option value="6">Снегоход, мотосани</option>
-              <option value="7">Катер, моторная лодка, другое водное ТС</option>
-              <option value="8">Яхта, другое парусно-моторное судно</option>
-              <option value="9">Гидроцикл</option>
-              <option value="10">Несамоходное (буксируемое) судно</option>
-            </select>
+            <select id="reg" class="form-control"></select>
           </div><!-- /input-group -->
           <div class="input-group">
             <span class="input-group-addon">Год</span>
-            <select id="nalog_year" class="form-control">
-              <option>2015</option>
-              <option>2016</option>
-              <option>2017</option>
-              <option selected>2018</option>
+            <select id="nalog_year" class="form-control"></select>
+          </div><!-- /input-group -->
+          <div class="input-group">
+            <span class="input-group-addon">Месяцев во владении</span>
+            <select id="month" class="form-control">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12" selected>12</option>
             </select>
+          </div><!-- /input-group -->
+          <div class="input-group">
+            <span class="input-group-addon">Транспортное средство</span>
+            <select id="auto_type" class="form-control"></select>
+          </div><!-- /input-group -->
+
+          <div class="input-group hidden">
+            <span class="input-group-addon">Марка транспортного средства</span>
+            <select id="auto_brand" class="form-control"></select>
+          </div><!-- /input-group -->
+          <div class="input-group hidden">
+            <span class="input-group-addon">Модель транспортного средства</span>
+            <select id="auto_model" class="form-control"></select>
+          </div><!-- /input-group -->
+          <div class="input-group hidden">
+            <span class="input-group-addon">Год выпуска транспортного средства</span>
+            <input id="model_year" type="number" class="form-control" placeholder="Год выпуска" >
           </div><!-- /input-group -->
 
           <div class="input-group">
             <span class="input-group-addon">Мощность</span>
             <input id="power" type="number" class="form-control" placeholder="мощность" value="100">
-            <span class="input-group-addon" id="input_group_ls">л.с.</span>
+          </div><!-- /input-group -->
+          <div class="input-group hidden">
+            <span class="input-group-addon">Льгота</span>
+            <select id="benefit" class="form-control">
+              <option value="0">Нет</option>
+            </select>
           </div><!-- /input-group -->
 
+          <button id="calculate" class="button">Рассчитать</button>
+
           <div id="result" class="transport-tax-box-result"></div>
+          <div id="warning" class="warning"></div>
 
         </div>
 
-        <table id="mainTable" class="table table-striped">
-          <thead>
-          <tr>
-            <th>Наименование объекта налогообложения</th>
-            <th>Ставка, руб</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr hidden="true">
-            <td>01</td>
-            <td></td>
-          </tr>
-          </tbody>
-        </table>
       </div>
       <div class="panel panel-success transport-tax-box-description">
         <div class="panel-heading">
